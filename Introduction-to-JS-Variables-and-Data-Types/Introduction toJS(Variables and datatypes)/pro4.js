3b9f74ba1f713bb04206a6aa9affb0e750ad7542
Nitesh Sharma <niteshsharma@Niteshs-MacBook-Pro.local>
2022-10-12 20:18:14 +0530

    masai assignment part

---
+++ Introduction-to-JS-Variables-and-Data-Types/Introduction toJS(Variables and datatypes)/pro4.js
@@ -0,0 +1,18 @@
+//Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+//Print the report card of the student (You can make it look nice by using some keyboard symbols )
+let topic="             Report Card\n"
+let school="         PRAGYAN PUBLIC SCHOOL\n"
+let name="Sakshi Sharma\n";
+let grade= "Grade : X"
+let section="Section : A"
+let roll_no="Roll No. : 20\n"
+let subject1_marks="Science : 90\n"
+let subject2_marks="Maths : 95\n"
+let subject3_marks ="English : 92"
+console.log(topic);
+console.log(school);
+console.log(name);
+console.log(grade,"       ",section,"        ",roll_no)
+console.log(subject1_marks)
+console.log(subject2_marks)
+console.log(subject3_marks)
